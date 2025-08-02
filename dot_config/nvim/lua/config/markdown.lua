e9e1809bc9dc382aa44780b63a0ac05a61ffde40
Furragen <git@axk.io>
2025-08-02 19:38:22 +0200

    add whole nvim config

---
+++ dot_config/nvim/lua/config/markdown.lua
@@ -0,0 +1,7 @@
+return {                                                                                                                                                                            │ │
+│ │   {                                                                                                                                                                                 │ │
+│ │     "MeanderingProgrammer/render-markdown.nvim",                                                                                                                                    │ │
+│ │     opts = {                                                                                                                                                                        │ │
+│ │       enabled = false, -- Disable on startup                                                                                                                                        │ │
+│ │     },
+  }

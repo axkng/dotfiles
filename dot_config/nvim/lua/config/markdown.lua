f10546134634cb5b9c6abf69b52464e69789b2f4
Furragen <git@axk.io>
2025-08-02 19:39:49 +0200

    remove unuused config

--- dot_config/nvim/lua/config/markdown.lua
+++
@@ -1,7 +0,0 @@
-return {                                                                                                                                                                            │ │
-│ │   {                                                                                                                                                                                 │ │
-│ │     "MeanderingProgrammer/render-markdown.nvim",                                                                                                                                    │ │
-│ │     opts = {                                                                                                                                                                        │ │
-│ │       enabled = false, -- Disable on startup                                                                                                                                        │ │
-│ │     },
-  }
